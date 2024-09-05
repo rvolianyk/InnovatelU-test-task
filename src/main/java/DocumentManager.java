f5293b96b46a6d92bc687d4eedbc874c5d78925d
Roman Volianyk <bober2642@gmail.com>
2024-09-05 23:31:00 +0300

    Base code

--- src/main/java/DocumentManager.java
+++ src/main/java/DocumentManager.java
@@ -2,13 +2,12 @@ import lombok.Builder;
 import lombok.Data;
 
 import java.time.Instant;
-import java.util.Collections;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * For implement this task focus on clear code, and make this solution as simple readable as possible
- * Don't worry about performance, concurrency, etc
+ * Don't worry about performance, concurrency, etc.
  * You can use in Memory collection for sore data
  * <p>
  * Please, don't change class name, and signature for methods save, search, findById
@@ -16,7 +15,8 @@ import java.util.Optional;
  * This class could be auto tested
  */
 public class DocumentManager {
-
+    private final Map<String, Document> documents = new HashMap<>();
+    private int id = 1;
     /**
      * Implementation of this method should upsert the document to your storage
      * And generate unique id if it does not exist, don't change [created] field
@@ -25,19 +25,37 @@ public class DocumentManager {
      * @return saved document
      */
     public Document save(Document document) {
-
-        return null;
+        if (document.getId() == null) {
+            while (documents.containsKey(String.valueOf(id))) {
+                id++;
+            }
+            document.setId(String.valueOf(id++));
+        }
+        documents.put(document.getId(), document);
+        return document;
     }
 
     /**
-     * Implementation this method should find documents which match with request
+     * Implementation of this method should find documents which match with request
      *
      * @param request - search request, each field could be null
      * @return list matched documents
      */
     public List<Document> search(SearchRequest request) {
+        if (request == null) return Collections.emptyList();
 
-        return Collections.emptyList();
+        return documents.values().stream()
+                .filter(doc -> request.getTitlePrefixes() == null ||
+                        request.getTitlePrefixes().stream().anyMatch(prefix -> doc.getTitle().startsWith(prefix)))
+                .filter(doc -> request.getContainsContents() == null ||
+                        request.getContainsContents().stream().anyMatch(content -> doc.getContent().contains(content)))
+                .filter(doc -> request.getAuthorIds() == null ||
+                        request.getAuthorIds().contains(doc.getAuthor().getId()))
+                .filter(doc -> request.getCreatedFrom() == null ||
+                        doc.getCreated().isAfter(request.getCreatedFrom()))
+                .filter(doc -> request.getCreatedTo() == null ||
+                        doc.getCreated().isBefore(request.getCreatedTo()))
+                .collect(Collectors.toList());
     }
 
     /**
@@ -47,8 +65,7 @@ public class DocumentManager {
      * @return optional document
      */
     public Optional<Document> findById(String id) {
-
-        return Optional.empty();
+        return Optional.ofNullable(documents.get(id));
     }
 
     @Data
